f698d483c1a64a0ce20c3c004e4ab21bb8c3d5b2
marlene32100 <katia.debon@hotmail.it>
2021-01-14 19:14:07 +0000

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 from products.models import Product
 
 
-# Create your models here.
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -23,6 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
